13066e3040735d40adddef5bb3e95973b7488125
Najeeb Said <tcanjb@gmail.com>
2023-04-06 22:11:04 +0500

    Final Hopefully

---
+++ getClassTimetables.js
@@ -0,0 +1,76 @@
+const puppeteer = require('puppeteer');
+const {JSDOM} = require("jsdom");
+const getClassesList = require("./getClassesList");
+
+const getClassTimetable = async (classes = []) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate to the website and wait for it to load
+  await page.goto('https://cuonline.cuiatd.edu.pk/Timetable/Timetable.aspx');
+  await page.waitForSelector('[name="ddlClasses"]');
+
+  let timeTables = {};
+  if (classes.length === 0) {
+    classes = await getClassesList();
+  }
+  for (let i = 0; i < classes.length; i++) {
+    console.log(classes[i])
+    // Change the select element to "BSE 6A" and fire the onchange event
+    await page.select('[name="ddlClasses"]', classes[i]);
+    await page.waitForSelector('#gvTimeTable1');
+
+    // Extract the table HTML content and output it to the console
+    const tableHtml = await page.$eval('#gvTimeTable1', table => table.outerHTML);
+
+    const dom = new JSDOM(tableHtml);
+    const table = dom.window.document.querySelector('table');
+    const header = table.querySelectorAll('th');
+    const time = [];
+    header.forEach((col, i) => {
+      if (i > 0) time.push(col.textContent);
+    });
+    const rows = table.querySelectorAll('tr');
+    const data = [];
+    rows.forEach(row => {
+      const cols = row.querySelectorAll('td');
+      const rowData = {};
+      let k = 0;
+      cols.forEach((col, i) => {
+        if (i === 0) {
+          rowData['Day'] = col.textContent;
+        } else if (col.innerHTML.split('<br>').length > 1) {
+          const info = extractInfo(col.innerHTML.toString());
+          if (col.colSpan > 1) {
+            rowData[time[k]] = info;
+            rowData[time[k + 1]] = info;
+            k += 2;
+          } else {
+            rowData[time[k]] = info;
+            k++;
+          }
+        } else {
+          rowData[time[k]] = null;
+          k++;
+        }
+      });
+      data.push(rowData);
+    });
+    timeTables[classes[i]] = data;
+  }
+  // console.log(data)
+  // Close the browser
+  await browser.close();
+  return timeTables;
+};
+
+function extractInfo(str) {
+  let info = str.split('<br>');
+  let subject = info[0];
+  let classRoom = info[1];
+  let teacher = info[2].split('<b>')[1].split('</b>')[0];
+  return {subject, classRoom, teacher};
+}
+
+module.exports = getClassTimetable;
+
