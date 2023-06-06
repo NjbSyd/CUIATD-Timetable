c6ef4dd36a2b7e36784c495aa1aeba85ea0e5815
Najeeb Said <tcanjb@gmail.com>
2023-06-06 20:16:46 +0500

    Changed .gitignore Final

--- Functions/RequestsHandlers.js
+++ Functions/RequestsHandlers.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
+
 module.exports = function getClassesTimetableFromLocalJSON(classes) {
-  const data = require("../Output/Timetable.json");
-  if (classes.length === 0) return data;
-  const timetable = {};
-  classes.forEach((className) => {
-    timetable[className] = data[className];
-  });
-  return timetable;
+  try {
+    const data = require("../Output/Timetable.json");
+    if (classes.length === 0) return data;
+    const timetable = {};
+    classes.forEach((className) => {
+      timetable[className] = data[className];
+    });
+    return timetable;
+  } catch (err) {
+    return `Timetable data not found. Please try again after 01:30 UTC`;
+  }
 }
