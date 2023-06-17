fdbd13e97928359388c152bd7dd8704423c6d91c
Najeeb Said <tcanjb@gmail.com>
2023-06-17 10:56:17 +0500

    Added Mongoose to CronJob and Refactored code

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const ScheduleCronJob = require("./Scheduler/Scheduler");
 const express = require("express");
-const {connectToMongoDB} = require("./MongoDB/MongoConfig");
-
-
 
 
 const app = express();
 app.use(express.json());
-connectToMongoDB();
 ScheduleCronJob();
-// app.use("/",router);
 app.listen(3000, () => {
-
-    console.log("Server Started on port 3000")
+  console.log("Server Started on port 3000")
 })
