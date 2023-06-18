dfa7c2002827214c988a5657136ab6af30e60384
Najeeb Said <tcanjb@gmail.com>
2023-06-18 14:07:36 +0500

    Resolved Small Bug

--- Scheduler/Scheduler.js
+++ Scheduler/Scheduler.js
@@ -4,15 +4,12 @@ const cron = require("node-cron");
 const { AddSchedule } = require("../MongoDB/StoreDataInMongoDB");
 
 const { extractTimetableData } = require("../Functions/DataManipulation");
-const {
-  connectToMongoDatabase,
-  disconnectFromMongoDatabase,
-} = require("../MongoDB/MongoConfig");
+const { connectToMongoDatabase } = require("../MongoDB/MongoConfig");
 
 const ExtractData = async () => {
   try {
     console.log("Scrapping Started");
-    const Timetables = await scrapClassTimetable(["BSE 6A"]);
+    const Timetables = await scrapClassTimetable();
     const ActualTimetable = extractTimetableData(Timetables);
     await connectToMongoDatabase();
     const promises = ActualTimetable.map(async (schedule) => {
@@ -20,11 +17,9 @@ const ExtractData = async () => {
     });
     await Promise.all(promises);
     storeLogs(false, "Scrapping completed Successfully");
-    await disconnectFromMongoDatabase();
   } catch (error) {
     console.log(error);
     storeLogs(true, error.message);
-    await disconnectFromMongoDatabase();
   }
 };
 
