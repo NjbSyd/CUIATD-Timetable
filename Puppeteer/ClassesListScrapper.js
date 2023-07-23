eda3cdd958708d0dab07e91f25fa901359684b1d
Najeeb Said <tcanjb@gmail.com>
2023-07-23 20:18:33 +0500

    Optimized Classes-List Extraction
    Leveraged Axios and Cheerio to replace Puppeteer for this specific module. Substantial improvement observed, with processing time reduced from 2-6 seconds to just 100-600 ms (based on network speed). 🚀

--- Puppeteer/ClassesListScrapper.js
+++ Puppeteer/ClassesListScrapper.js
@@ -1,21 +1,37 @@
-const puppeteer = require("puppeteer");
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 /*
  extracts the list of classes from the website
  @returns an Array: ["BSE 6A", "BSE 6B", ...]
  */
 const CourseSemesterSectionScrapper = async () => {
+  const url = 'https://cuonline.cuiatd.edu.pk/Timetable/Timetable.aspx';
+
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // Navigate to the website and wait for it to load
-    await page.goto('https://cuonline.cuiatd.edu.pk/Timetable/Timetable.aspx');
-    await page.waitForSelector('[name="ddlClasses"]');
-    const classes = await page.$$eval('[name="ddlClasses"] option', options => options.map(option => option.value));
-    await browser.close();
-    return classes;
+    const response = await axios.get(url);
+    console.log(response.status)
+    if (response.status !== 200) {
+      throw new Error(`Request failed with status code ${response.status}`);
+    }
+
+    return extractClassesFromHTML(response.data);
   } catch (error) {
     throw new Error(error.message);
   }
-}
+};
+
+const extractClassesFromHTML = (html) => {
+  const classes = [];
+  const $ = cheerio.load(html);
+  const options = $('[name="ddlClasses"] option');
+
+  options.each((_, element) => {
+    const classValue = $(element).attr('value');
+    classes.push(classValue);
+  });
+  console.log(classes)
+  return classes;
+};
+
 module.exports = CourseSemesterSectionScrapper;
