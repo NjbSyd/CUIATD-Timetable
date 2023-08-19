82822c6418c6847dba10c2c4aec3c0dc0991b49e
Najeeb Said <tcanjb@gmail.com>
2023-08-20 01:23:23 +0500

    Added Logs path and FreeSlots.js
    Now freeslots can be retrieved from MongoDB using /freeslots path

--- Scheduler/StoreLogs.js
+++ Scheduler/StoreLogs.js
@@ -2,8 +2,10 @@ const fs = require("fs");
 
 exports.storeLogs = (isError, errorMessage) => {
   try {
-    const log = `${isError ? "Failure" : "Success"}: ${new Date()}: ${errorMessage}\n`;
-    fs.appendFileSync("./Scheduler/cronjob-logs", log);
+    const log = `${
+      isError ? "Failure" : "Success"
+    }: ${new Date()}: ${errorMessage}\n`;
+    fs.appendFileSync("./Scheduler/cronjob-logs.txt", log);
   } catch (error) {
     console.log(error);
   }
