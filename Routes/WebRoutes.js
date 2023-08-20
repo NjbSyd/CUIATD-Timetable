23565e7edb2c301e428c1871b16dd069bb5c16a2
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-08-20 13:52:22 +0500

    added some validation middlewares

--- Routes/WebRoutes.js
+++ Routes/WebRoutes.js
@@ -1,10 +1,11 @@
 const { getDropdownData, getTeacherSchedule, getSubjectSchedule, getClassSchedule, getClassRoomSchedule } = require("../MongoDB/WebRoutesHandler");
 const Router = require("express").Router();
+const {teacherValidator,subjectValidator,roomValidator,classValidator}=require("../middleware/validator")
 Router.get("/", getDropdownData);
-Router.get("/teacher/:teacherName", getTeacherSchedule)
-Router.get("/subject/:subjectName", getSubjectSchedule)
-Router.get("/class/:className", getClassSchedule)
-Router.get("/room/roomName/:roomName/timeSlot/:timeSlot/day/:day", getClassRoomSchedule)
+Router.get("/teacher/:teacherName",teacherValidator, getTeacherSchedule)
+Router.get("/subject/:subjectName",subjectValidator, getSubjectSchedule)
+Router.get("/class/:className",classValidator, getClassSchedule)
+Router.get("/roomName/:roomName/timeSlot/:timeSlot/day/:day",roomValidator, getClassRoomSchedule)
 
 module.exports = Router;
 
