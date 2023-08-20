7f7017283af51d9099248f73c80700ac5bc84818
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-08-20 13:52:22 +0500

    added some validation middlewares

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const ScheduleCronJob = require("./Scheduler/Scheduler");
 const express = require("express");
 const { connectToMongoDatabase } = require("./MongoDB/MongoConfig");
 const app = express();
+const cors=require("cors")
 
 app.use(express.json());
+
+app.use(cors(
+  {
+origin: ['http://localhost:5173'],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }
+
+))
+
 
 app.use("/timetable", require("./Routes/TimetableData"));
 app.use("/quotes", require("./Routes/Quotes"));
